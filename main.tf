37b45405537857900547a8916611a2e341743233
sowjiarha <sowjiarhana@gmail.com>
2023-04-29 23:08:27 +0530

    changes

--- main.tf
+++ main.tf
@@ -12,10 +12,10 @@ terraform {
   required_version = ">= 1.1.0"
 
   cloud {
-    organization = "REPLACE_ME"
+    organization = "hashi-crop-sowjiarha"
 
     workspaces {
-      name = "learn-terraform-github-actions"
+      name = "githubactions-terraform-demo"
     }
   }
 }
